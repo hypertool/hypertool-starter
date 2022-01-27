5087074f543bbf839d5657311e5c194257a39cb0
Samuel Rowe <samuelrowe1999@gmail.com>
2022-01-27 13:03:25 +0530

    feat: updated `PrimaryLayout` component

--- source/components/common/PrimaryLayout.tsx
+++ source/components/common/PrimaryLayout.tsx
@@ -1,34 +1,76 @@
 import type { FunctionComponent, ReactElement } from "react";
 
-import { AppBar, Toolbar, Button, Typography } from "@mui/material";
+import { Toolbar, Button, Typography, IconButton, Icon } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import React from "react";
 
+import Drawer from "./Drawer";
+import Main from "./Main";
+import AppBar from "./AppBar";
+import configuration from "../../configuration.json";
+
 const Root = styled("div")(({ theme }) => ({}));
 
-const Main = styled("main")(({ theme }) => ({
-    marginTop: theme.spacing(8),
+const Logo = styled(Button)(({ theme }) => ({
+    height: 64,
+}));
+
+const LogoImage = styled("img")(({ theme }) => ({
+    height: 24,
+    width: "auto",
+    marginRight: theme.spacing(1),
+}));
+
+const LogoText = styled(Typography)(({ theme }) => ({
+    textTransform: "none",
+    color: "white",
+    fontWeight: 600,
 }));
 
 interface Props {
     children: ReactElement;
 }
 
+const groups = [];
+
 const PrimaryLayout: FunctionComponent<Props> = (
     props: Props,
 ): ReactElement => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
     return (
         <Root>
-            <AppBar>
+            <AppBar open={open}>
                 <Toolbar>
-                    <Button>
-                        <Typography style={{ color: "white" }}>
-                            Hypertool App
-                        </Typography>
-                    </Button>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{ mr: 2, ...(open && { display: "none" }) }}
+                    >
+                        <Icon>menu</Icon>
+                    </IconButton>
+                    <Logo>
+                        <LogoImage src="https://res.cloudinary.com/hypertool/image/upload/v1642502111/hypertool-starter/hypertool-logo_xvqljy.png" />
+                        <LogoText>Hypertool</LogoText>
+                    </Logo>
                 </Toolbar>
             </AppBar>
-            <Main>{props.children}</Main>
+            <Drawer
+                open={open}
+                handleDrawerClose={handleDrawerClose}
+                groups={configuration.layouts.primary.drawer}
+            />
+            <Main open={open}>{props.children}</Main>
         </Root>
     );
 };
